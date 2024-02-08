c307ac0db8870cbab2dec8aa54172c4fc41159e6
Lorena Goez <loregoez3@gmail.com>
2024-02-08 10:51:26 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to my Home Page</h1>')
+    return render(request, 'home.html', {'name':'Greg Lim'})
+
+def about(request):
+    return HttpResponse('<h1>This is the About page</h1>')
