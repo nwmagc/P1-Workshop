197901e76a048f4b53b4ffcd334d12ddc3cb3ea4
nwmagc <loregoez3gmail.com>
2024-03-02 00:32:15 -0500

    workshop2

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -16,14 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path
+from django.urls import path, include
 from movie import views as movieViews
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', movieViews.home),
-    path('about/', movieViews.about),
+    path('', movieViews.home, name = 'home'),
+    path('about/', movieViews.about, name='about'),
+    path('news/', include('news.urls')),
+    path('statistics/', movieViews.statistics_views, name='statistics'),
+    path('signup/', movieViews.signup, name= 'signup'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
